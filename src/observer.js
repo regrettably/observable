f2c314ba8fcc3c2659523775936518afd7cc977e
Ben Ford <ben@ford.sh>
2018-07-04 01:31:23 -0500

    Rename variable & more elegant value handling

--- src/observer.js
+++ src/observer.js
@@ -1,31 +1,30 @@
 function Observer(observable) {
-    let value = observable.value
-    let observableIndex
+    let value
+    let observerIndexPosition
     let registeredCallback
-    function update(observed) {
-        if (registeredCallback) {
-            value = registeredCallback(observed.value)
-        } else {
-            value = observed.value
-        }
+    function update() {
+        return this.value
     }
     this.subscribe = (after) => {
-        observableIndex = observable.register({ update, after })
+        observerIndexPosition = observable.register({ update, after })
         return this
     }
     this.unsubscribe = (finalValue) => {
+        observable.unregister(observerIndexPosition)
         observable = undefined
         value = finalValue
-        observable.unregister(observableIndex)
     }
     this.then = (callback) => {
         registeredCallback = callback
-        value = update(observable.value)
         return this
     }
 
     Object.defineProperty(this, 'value', {
-        get: () => value,
+        get: () => {
+            if (!observable) return value
+            if (registeredCallback) return value = registeredCallback(observable.value)
+            return value = observable.value
+        },
     })
 }
 
